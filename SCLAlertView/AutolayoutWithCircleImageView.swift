f1c8115f46cac245a54d3651fa28db1530c99393
Sergey Mikhalchenko <serge.mikhalchenko@gmail.com>
2016-02-18 19:41:54 +0200

    Fix bugs

--- SCLAlertView/AutolayoutWithCircleImageView.swift
+++ SCLAlertView/AutolayoutWithCircleImageView.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 
-class AutolayoutCircleImageView: UIImageView {
+class AutolayoutWithCircleImageView: UIImageView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
